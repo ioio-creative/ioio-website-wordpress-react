d8b6e974e3b7bf656fce67b9faee080c79040d0c
Kenny Wong <kennyviperhk@gmail.com>
2018-04-18 20:59:16 +0800

    project layout update on testpage

--- src/TestPage/IOIO/js/main.js
+++ src/TestPage/IOIO/js/main.js
@@ -108,7 +108,7 @@ jQuery(document).ready(function($) {
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({autoplay: true, dots: true, loop: true, items: 1});
+  $(".slideshow-carousel").owlCarousel({autoplay: true, dots: true, loop: true, items: 1});
 
   //#Sidebar
   $("#menu-close").click(function(e) {
