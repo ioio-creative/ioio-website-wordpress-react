e1bfd8f4075a16fee029b80f71e1d3d915224591
Kenny Wong <kennyviperhk@gmail.com>
2018-04-28 02:25:17 +0800

    update

--- src/pages/ProjectDetailPage.js
+++ src/pages/ProjectDetailPage.js
@@ -83,6 +83,9 @@ Modal.setAppElement('#root');
 
 function VideoLanding(props) {
   const video_url = props.project.cover_video.guid;
+  const video_webm_url = props.project.cover_video.guid;
+  const poster_url = props.project.thumbnail.guid;
+
   const full_url = [video_url,'https://i.gifer.com/3hmW.gif'];
   const video_url_shorten = video_url.replace(".mp4", "") //TODO use replace ''
   const data_vid = 'mp4:' + video_url_shorten + ', webm: video/ocean, ogv:' + video_url_shorten + ', poster: video/ocean" data-vide-options="position: 0% 50%'
@@ -91,7 +94,7 @@ function VideoLanding(props) {
     <div className="video-landing-div">
       <div className="container-fluid ">
         <div className="player-wrapper">
-          <video className="react-player" width={'100%'} height={'auto'} poster="https://media.giphy.com/media/5CIRqPOk0141W/giphy.gif" autoplay={"autoplay"} loop={"loop"} muted playsinline={"playsinline"}>
+          <video className="react-player" width={'100%'} height={'auto'} poster={poster_url} autoplay={"autoplay"} loop={"loop"} muted playsinline={"playsinline"}>
           <source src={video_url} type="video/mp4" />
           <source src="https://multicdn.synq.fm/projects/bb/56/bb56f28429b942c08dc5128e4b7ba48c/derivatives/videos/71/43/71439ccd73c74ecc8bbab7abd3bb98bc/webm_720/71439ccd73c74ecc8bbab7abd3bb98bc_webm_720.webm" type="video/webm" />
           </video>
