1c4558a2a7a90986edb74887af10d5a30d58a22b
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-05-08 19:05:08 +0800

    fixed ProjectTemp07.js

--- src/containers/projectDetail/ProjectTemp07.js
+++ src/containers/projectDetail/ProjectTemp07.js
@@ -18,7 +18,7 @@ class ProjectTemp07 extends Component {
               <video className="photo-montage-one-vid-inside" width={'100%'} height={'auto'} poster={props.images[0].guid} autoPlay={"autoPlay"} loop={"loop"} muted="muted" playsInline={"playsInline"}>
                 <source src={props.video.guid} type="video/mp4"/>
               {/* //TODO add webm <source src="https://multicdn.synq.fm/projects/bb/56/bb56f28429b942c08dc5128e4b7ba48c/derivatives/videos/71/43/71439ccd73c74ecc8bbab7abd3bb98bc/webm_720/71439ccd73c74ecc8bbab7abd3bb98bc_webm_720.webm" type="video/webm"/>*/}
-                <img src={props.images[0].guid} title="Your browser does not support the <video> tag" />
+                {/*<img src={props.images[0].guid} title="Your browser does not support the <video> tag" />*/}
               </video>
             </div>
           </section>
