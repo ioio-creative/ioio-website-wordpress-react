9b8ed87c91e15988534ff572569ea4d438299546
Kenny Wong <kennyviperhk@gmail.com>
2018-06-05 18:22:53 +0800

    update before make branch

--- src/pages/LabListPage.js
+++ src/pages/LabListPage.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
 
+//Footer
+import Footer from 'containers/Footer';
+import {fetchActiveFooter} from 'websiteApi';
+
+
+import './LabListPage.css';
+
+
 class LabListPage extends Component {
   constructor(props) {
     super(props);
@@ -8,14 +16,51 @@ class LabListPage extends Component {
     }
   }
 
-  componentDidMount() {
-
+  async componentDidMount() {
+    fetchActiveFooter((aFooter) => {
+      this.setState({footer: aFooter});
+    });
   }
 
   render() {
+    //var a = this.props.about
+
+  //  const url = "url('" + a.about_top_section_background_image.guid + "')"
+
+    const bg = {
+      //backgroundImage: url,
+      backgroundSize: 'auto',
+      backgroundPosition: 'center'
+    };
+
+
+    const footer = this.state.footer;
+    if (footer === null) {
+      return null;
+    }
+
     return (
       <div>
-        This is lab list page!
+        <section id="lab-top" className="wow fadeIn lab-section-bg" style={bg}>
+          <div className="container-fluid row text-left">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+              <div className="about01-img text-right">
+                <img id="img-hotpot" src={a.hotpot_image.guid} alt="" className="img-fluid"/>
+                <img id="img-meet" src={a.meat_image.guid} alt="" className="img-fluid"/>
+                <img id="img-fish" src={a.fish_image.guid} alt="" className="img-fluid"/>
+              </div>
+              <h3>{a.page_title}</h3>
+              <h1>{a.page_subtitle}</h1>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+        </section>
+        <section id="lab-list">
+
+        </section>
+        <Footer
+          footer={footer}/>
       </div>
     );
   }
