a4c50d113f90ffc2d2ef509d280e4906c6076c2a
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-06-14 16:13:51 +0800

    added projectsByCategory in routes.js

--- src/globals/routes.js
+++ src/globals/routes.js
@@ -4,20 +4,23 @@
 */
 
 const routes = {
-    home: "/",
-    about: "/about",
-    projects: "/projects",
-    projectBySlug: "/projects/:projectSlug",
-    projectBySlugWithValue: (slugValue) => {
-        return "/projects/" + slugValue;
-    },
-    lab: "/lab",
-    labBySlug: "/lab/:labSlug",
-    labBySlugWithValue: (slugValue) => {
-        return "/lab/" + slugValue;
-    },
-    contacts: "/contacts",
-    notFound: "/notfound"
+  home: "/",
+  about: "/about",
+  projects: "/projects",
+  projectsByCategory: (categorySlug) => {
+    return "/projects?category=" + categorySlug;
+  },
+  projectBySlug: "/projects/:projectSlug",
+  projectBySlugWithValue: (slugValue) => {
+    return "/projects/" + slugValue;
+  },
+  lab: "/lab",
+  labBySlug: "/lab/:labSlug",
+  labBySlugWithValue: (slugValue) => {
+    return "/lab/" + slugValue;
+  },
+  contacts: "/contacts",
+  notFound: "/notfound"
 }
 
 export default routes;
