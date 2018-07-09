0ca346e1dfdb25bce0520ddb6b7fe24bde8f6ee0
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-07-09 11:38:30 +0800

    Improved GA Filter Projects By Category event action name

--- src/utils/reactGa/trackProjectListPageFilterByCategory.js
+++ src/utils/reactGa/trackProjectListPageFilterByCategory.js
@@ -1,9 +1,11 @@
 import trackEvent from './trackEvent';
 
+// Action and Label best practices
+// https://support.google.com/analytics/answer/1033068?hl=en
 export default function trackProjectListPageFilterByCategory(categorySlug) {
   trackEvent({
     category: 'Project List Page',
-    action: 'Filter Project by Category',
+    action: `Filter Projects by Category - ${categorySlug}`,
     label: `Projects - ${categorySlug}`,
     //value: value,
     //nonInteraction: nonInteraction,
