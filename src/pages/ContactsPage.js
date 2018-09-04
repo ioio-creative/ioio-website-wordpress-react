861e4b633379f1359a91cc3b57f5986628f00585
hung <hungchuncheung@gmail.com>
2018-09-04 17:13:27 +0800

    Facebook sharing image test
    Test facebook sharing image on page Contact

--- src/pages/ContactsPage.js
+++ src/pages/ContactsPage.js
@@ -62,7 +62,18 @@ class ContactsPage extends Component {
     if (contact === null) {
       return null;
     }
-
+    if (contact.facebook_og_images && contact.facebook_og_images.length) {
+      var ogimage = document.querySelectorAll('[property="og:image"]');
+      contact.facebook_og_images.forEach(ogimgobj => {
+        var newmeta = document.createElement('meta');
+        newmeta.setAttribute('property', 'og:image');
+        newmeta.setAttribute('content', ogimgobj.guid);
+        ogimage[0].parentNode.insertBefore(newmeta,ogimage[0]);
+      });
+      ogimage.forEach(oldmeta => {
+        oldmeta.parentNode.removeChild(oldmeta);
+      })
+    }
     return (
       <div>
         <section id="contact-top" className="wow contact-section-bg">
