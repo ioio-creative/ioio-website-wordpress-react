4e2a4a7dd21e1fed2f1cdca25f06d4974e70bae7
hcc1232001 <qes4d26@hotmail.com>
2019-04-16 18:21:24 +0800

    update
    template10
    - video autoplay added

--- src/containers/projectDetail/ProjectTemp10.js
+++ src/containers/projectDetail/ProjectTemp10.js
@@ -3,18 +3,44 @@ import React, { Component } from 'react';
 class ProjectTemp10 extends Component {
     constructor(props) {
       super(props);
+      this.video = null;
+      this.playButton = null;
+      this.hidePlayButton = this.hidePlayButton.bind(this);
+      this.showPlayButton = this.showPlayButton.bind(this);
+      this.playVideo = this.playVideo.bind(this);
     }
-
+    componentDidMount() {
+      if (this.props.is_autoplay_video === "1")
+        this.playVideo();
+      else 
+        this.showPlayButton();
+    }
+    hidePlayButton() {
+      this.playButton.style.visibility = 'hidden';
+      this.playButton.style.opacity = 0;
+      this.video.controls = true;
+    }
+    showPlayButton(e) {
+      /* https://stackoverflow.com/questions/40584563/html5-video-fires-pause-event-while-seeked */
+      if (this.video.readyState === 4) {
+        this.playButton.style.visibility = 'visible';
+        this.playButton.style.opacity = 1;
+        this.video.controls = false;
+      }
+    }
+    playVideo() {
+      this.video.play();
+    }
     render() {
       const props = this.props;
       const bg = {
         backgroundColor: props.background_mood_color,
       };
-      const isAutoPlay = {
+      const videoAttrs = {
         autoPlay: (props.is_autoplay_video === "1"? true: false),
-        controls: true
+        loop: (props.is_autoplay_video === "1"? true: false)
       }
-      console.log(isAutoPlay);
+      // console.log(videoAttrs);
       return (
         <section className="photo-montage-one-video project-section-bg wow fadeInUp" style={bg}>
           {/* TODO: modified by Chris */}
@@ -22,8 +48,15 @@ class ProjectTemp10 extends Component {
             <div className="row container-fluid">
               <div className="col-md-10 offset-md-1">
                 <div className="video-wrapper">
-                  <video src={props.video.guid} {...isAutoPlay} />
-                  {!isAutoPlay.autoPlay && <div className="play-button" />}
+                  <video src={props.video.guid} {...videoAttrs}
+                    onPlay={this.hidePlayButton}
+                    onPause={this.showPlayButton}
+                    ref={ref=>this.video=ref}
+                  />
+                  <div className="play-button"
+                    onClick={this.playVideo}
+                    ref={ref=>this.playButton=ref}
+                  />
                 </div>
               </div>
             </div>
